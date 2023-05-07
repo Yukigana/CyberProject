64f0a2cb930d7bbdb814463a34750d482ebdfff3
Valentin <valentin.debonnefoy@gmail.com>
2023-05-07 17:39:27 +0200

    maj templates

--- src/Controller/OverviewController.php
+++ src/Controller/OverviewController.php
@@ -5,20 +5,38 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
-#[Route('/', name: 'overview')]
+#[Route('', name: 'overview')]
 class OverviewController extends AbstractController
 {
-    #[Route('', name: '_accueil')]
+    #[Route('/', name: '')]
+    public function defaultAction(): Response
+    {
+        return new RedirectResponse($this->generateUrl('overview_accueil'));
+    }
+    
+    #[Route('/accueil', name: '_accueil')]
     public function accueilAction(): Response
     {
         return $this->render('Overview/Accueil.html.twig');
     }
 
+    #[Route('/info', name: '_info')]
+    public function infoAction(): Response
+    {
+        return $this->render('Overview/Information.html.twig');
+    }
 
     #[Route('/aventure', name: '_aventure')]
     public function aventureAction(): Response
     {
         return $this->render('Overview/Aventure.html.twig');
+    }
+
+    #[Route('/duel', name: '_duel')]
+    public function duelAction(): Response
+    {
+        return $this->render('Overview/Duel.html.twig');
     }
 }
